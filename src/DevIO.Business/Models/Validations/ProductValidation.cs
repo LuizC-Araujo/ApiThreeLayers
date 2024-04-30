651e64a59f81667275cf72e8f7d866f68c4b6439
Luiz Araujo <arauldesign@gmail.com>
2024-04-30 13:06:44 -0300

    feature: adding product validation

---
+++ src/DevIO.Business/Models/Validations/ProductValidation.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models.Validations
+{
+    public class ProductValidation : AbstractValidator<Product>
+    {
+        public ProductValidation() 
+        {
+            RuleFor(r => r.Name)
+                .NotEmpty()
+                .WithMessage("O campo {PropertyName} deve ser fornecido!")
+                .Length(2, 200)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+
+            RuleFor(r => r.Description)
+                .NotEmpty()
+                .WithMessage("O campo {PropertyName} deve ser fornecido!")
+                .Length(2, 1000)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+
+            RuleFor(r => r.Value)
+                .GreaterThan(0).WithMessage("O campo {PropertyName} precisa ser maior que {ComparisonValue}!");
+        }
+    }
+}
