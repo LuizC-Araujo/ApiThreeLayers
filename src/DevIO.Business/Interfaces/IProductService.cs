8cfef7702e9b7a52407674ccc8de4226667e9702
Luiz Araujo <arauldesign@gmail.com>
2024-04-30 14:44:46 -0300

    feature: defining services interfaces

---
+++ src/DevIO.Business/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IProductService : IDisposable
+    {
+        Task Add(Product product);
+        Task Update(Product product);
+        Task Remove(Guid id);
+    }
+}
---
+++ src/DevIO.Business/Interfaces/ISupplierService.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface ISupplierService : IDisposable
+    {
+        Task Add(Supplier supplier);
+        Task Update(Supplier supplier);
+        Task Remove(Guid id);
+    }
+}
