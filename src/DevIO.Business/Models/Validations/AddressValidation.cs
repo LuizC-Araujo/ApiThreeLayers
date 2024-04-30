06b57bdfc35903cd80feae2c4d1050254bf2be4f
Luiz Araujo <arauldesign@gmail.com>
2024-04-30 13:13:55 -0300

    feature: adding address validation

---
+++ src/DevIO.Business/Models/Validations/AddressValidation.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models.Validations
+{
+    public class AddressValidation : AbstractValidator<Address>
+    {
+        public AddressValidation() 
+        {
+            RuleFor(r => r.Logradouro)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido!")
+                .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+
+            RuleFor(r => r.Neighborhood)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido!")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+
+            RuleFor(r => r.Cep)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido!")
+                .Length(8).WithMessage("O campo {PropertyName} precisa ter {MaxLength} caracteres!");
+
+            RuleFor(r => r.City)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido!")
+                .Length(2, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+
+            RuleFor(r => r.State)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido!")
+                .Length(2, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+
+            RuleFor(r => r.Number)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido!")
+                .Length(2, 50).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres!");
+        }
+    }
+}
