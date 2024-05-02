2810309b3928a68bb54ec44bd16626e2a51e3cc0
Luiz Araujo <arauldesign@gmail.com>
2024-05-02 16:54:37 -0300

    feature: definig generi repository

---
+++ src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,63 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly DevIODbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository(DevIODbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public virtual async Task<List<TEntity>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetById(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task Add(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Delete(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+    }
+}
