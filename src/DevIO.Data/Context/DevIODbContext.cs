9bcb98ee63edbe489bad92c34a09f87afe639ef1
Luiz Araujo <arauldesign@gmail.com>
2024-05-02 14:21:56 -0300

    feature: adding dbcontext and ef

---
+++ src/DevIO.Data/Context/DevIODbContext.cs
@@ -0,0 +1,17 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class DevIODbContext : DbContext
+    {
+        public DevIODbContext(DbContextOptions<DevIODbContext> options) : base(options) 
+        {
+            // some configs
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Adresses { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
