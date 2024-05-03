83628505a305dfcd3c9f15b9e67c1457a2105191
Luiz Araujo <arauldesign@gmail.com>
2024-05-03 11:55:48 -0300

    feature: defining main, product and supplier controllers

---
+++ src/DevIO.Api/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using DevIO.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : MainController
+    {
+        public ProductsController()
+        {           
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductViewModel>> Getall()
+        {
+
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProductViewModel>> GetById(Guid id)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductViewModel>> Add(ProductViewModel productViewModel)
+        {
+
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, ProductViewModel productViewModel)
+        {
+
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProductViewModel>> Delete(Guid id)
+        {
+
+        }
+    }
+}
---
+++ src/DevIO.Api/Controllers/SuppliersController.cs
@@ -0,0 +1,44 @@
+﻿using DevIO.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/suppliers")]
+    public class SuppliersController : MainController
+    {
+        public SuppliersController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<SupplierViewModel>> GetAll()
+        {
+
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<SupplierViewModel>> GetById(Guid id)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SupplierViewModel>> Add(SupplierViewModel supplierViewModel)
+        {
+
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<SupplierViewModel>> Update(Guid id, SupplierViewModel supplierViewModel)
+        {
+
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<SupplierViewModel>> Delete(Guid id)
+        {
+
+        }
+    }
+}
