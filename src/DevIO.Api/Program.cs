1a67ac34dd89dd32fd18cc5eb8c6b223f5579948
Luiz Araujo <arauldesign@gmail.com>
2024-05-03 10:02:37 -0300

    fix: cleaning api layer

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -3,10 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +16,6 @@ builder.Services.AddDbContext<DevIODbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
