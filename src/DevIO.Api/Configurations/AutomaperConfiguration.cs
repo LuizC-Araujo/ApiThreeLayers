3f6cfee9f8739e4a1dad4748fa46487812c01513
Luiz Araujo <arauldesign@gmail.com>
2024-05-03 12:15:31 -0300

    feature: implementing mappers

---
+++ src/DevIO.Api/Configurations/AutomaperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+
+namespace DevIO.Api.Configurations
+{
+    public class AutomaperConfiguration : Profile
+    {
+        public AutomaperConfiguration()
+        {
+            CreateMap<Supplier, SupplierViewModel>().ReverseMap();
+            CreateMap<Address, AddressViewModel>().ReverseMap();
+            CreateMap<ProductViewModel, Product>();
+
+            CreateMap<Product, ProductViewModel>()
+                .ForMember(dest => dest.SupplierName, opt => opt.MapFrom(src => src.Supplier.Name));
+        }
+    }
+}
--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<DevIODbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
